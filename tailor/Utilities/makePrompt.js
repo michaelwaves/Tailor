2de868f89633a74c47703a347659abea5e1811ec
michaelwaves <68243408+michaelwaves@users.noreply.github.com>
2023-02-11 15:28:31 -0500

    carousel 2

--- tailor/Utilities/makePrompt.js
+++ tailor/Utilities/makePrompt.js
@@ -4,11 +4,11 @@ function makePrompt(arr) {
     prompt += "with the size " + arr[1] + " "
     if (arr[2]) prompt += "and with the background " + arr[2] + " "
     if (arr[3]) prompt += "and with the style " + arr[3] + " "
-    if (arr[4]) prompt += "and with the gendre " + arr[4] + " "
+    if (arr[4]) prompt += "and with the gender " + arr[4] + " "
     //if (arr[5]) prompt += "and with the color " + arr[5] + " "
     // Keep adding to the prompt if there are more elements in the array
     // ...
-    prompt += "."   
+    prompt += "."
 
     return prompt
 }
--- tailor/app/routes/components/SizeMenu.jsx
+++ tailor/app/routes/components/SizeMenu.jsx
@@ -1,14 +1,13 @@
-import downArrow from "../images/drop-down-minor.svg"
+import rightArrow from "../images/right-arrow-solid.svg"
 import { useState } from "react"
 
 export default function SizeMenu(props) {
-    const [open, setOpen] = useState(false)
+
     return (
         <div id="choose-size" className="flex flex-row rounded-md bg-primary px-1.5 text-base font-semibold leading-7 text-white shadow-sm hover:bg-primary-light">
-            <button onClick={() => setOpen(!open)}>Size</button>
-            <input type="image" src={downArrow} alt="remove button" className="w-6 h-6" />
-
-            {open && props.children}
+            <button >Size</button>
+            <input type="image" src={rightArrow} alt="expand button" onClick={() => props.setOpen(!props.open)} className="w-5 h-5 self-center justify-self-end ml-3"></input>
+            {props.open && props.children}
         </div>
     )
 }
